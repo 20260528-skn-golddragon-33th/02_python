6d4ab09f75cf36f1bc21072e39704ad0b6dd95a8
golddragon0926 <102494942+golddragon0926@users.noreply.github.com>
2026-06-10 17:49:03 +0900

    Add files via upload

---
+++ _10_streamlit/_01_basics/_04_media.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("Media - image")
+
+# 서버 이미지
+st.image("../data/kindred.png", caption="champ")
+st.image("../data/Udyr.png", caption="champ")
+
+# 웹 이미지
+st.image("https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png", caption="google")
